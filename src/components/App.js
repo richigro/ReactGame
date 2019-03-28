99c32180651e802a102921b3b2542f63564f07fe
Ricardo Guerrero <richi_gro@hotmail.com>
2019-03-27 19:48:37 -0500

    creating new components

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Button1 from './button1';
+
+export default function App(props) {
+    return (
+        <main className="main">
+            <Button1 />
+        </main>
+    );
+}
---
+++ src/components/whatButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './button1.css';
+
+export default function Button1(props) {
+    return(
+        <div className="button1-box">
+            <button className="button1-btn">WHAT?</button>
+        </div>
+    );
+}
